cac20c35f07468c7b2eea750f3ad8dc837d34ced
Väinö Kurula <vaino.kurula@hotmail.com>
2024-01-24 23:03:54 +0200

    Added implied volatility to returned data

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import yfinance as yf
 
 app = Flask(__name__)
+
+def fetch_treasury_yield(maturity='10y'):
+    maturity_symbols = {
+        '1m': '^IRX',  # 1 month
+        '2y': '^FVX',  # 2 years
+        '5y': '^FVX',  # 5 years
+        '10y': '^TNX',  # 10 years
+        '30y': '^TYX'  # 30 years
+    }
+
+    symbol = maturity_symbols.get(maturity, '^TNX')  # Default to 10-year if maturity not found
+    history = yf.download(symbol, period='5d')
+
+    if not history.empty:
+        last_yield = history['Close'].iloc[-1]
+        risk_free_rate = last_yield / 100
+        return risk_free_rate
+    else:
+        return None
+
 
 def fetch_option_data(ticker_symbol):
     ticker = yf.Ticker(ticker_symbol)
@@ -19,6 +39,7 @@ def fetch_option_data(ticker_symbol):
                 "strikePrice": row['strike'],
                 "expirationDate": first_exp_date,
                 "marketPrice": row['lastPrice'],
+                "impliedVolatility": row['impliedVolatility'], # Added implied volatility
                 "quantity": None,
                 "optionValue": None
             }
@@ -30,6 +51,16 @@ def fetch_option_data(ticker_symbol):
 def options(ticker_symbol):
     data = fetch_option_data(ticker_symbol.upper())
     return jsonify(data)
+
+@app.route('/treasury_yield')
+def treasury_yield_endpoint():
+    maturity = request.args.get('maturity', '1m')  # Defaults to 10 years if not provided
+    yield_data = fetch_treasury_yield(maturity)
+
+    if yield_data is not None:
+        return jsonify({"maturity": maturity, "treasury_yield": yield_data})
+    else:
+        return jsonify({"error": "Yield data not available for this maturity"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
